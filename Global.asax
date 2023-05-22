1c9d6bc630bd87f42af20be006632d89cbc23d2c
HanNguyetPhan <hannguyetphan@gmail.com>
2023-05-22 15:26:14 +0700

    upload source code

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineToyStore.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineToyStore.Startup))]
+namespace OnlineToyStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
